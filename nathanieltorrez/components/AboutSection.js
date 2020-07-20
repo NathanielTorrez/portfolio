c6e0d317dda5c8e7113a00c85662b04d682b950d
Nathaniel Torrez <mr.realestatenate@gmail.com>
2020-07-20 14:39:11 -0700

    updated css layout for about section

--- nathanieltorrez/components/AboutSection.js
+++ nathanieltorrez/components/AboutSection.js
@@ -18,7 +18,14 @@ export default function AboutSection() {
     <>
       <section className="aboutContainer">
         <h1 className="title">About</h1>
-        <img src="/bio1.jpg" className="bioImg"></img>
+        <div className="bioAndSkillsContainer">
+          <img src="/bio1.jpg" className="bioImg"></img>
+          <div className="skillsContainer">
+            {softwares.map((arr, index) => (
+              <SkillsCard photo={arr[1]} software={arr[0]} key={index} />
+            ))}
+          </div>
+        </div>
         <p className="bio">
           That's me Nathaniel Torrez and I am a full stack software engineer
           with passion for making peoples lives easier or more fun through the
@@ -26,11 +33,6 @@ export default function AboutSection() {
           technologies very fast, below is an interactice list of strongest
           skills!
         </p>
-        <div className="skillsContainer">
-          {softwares.map((arr, index) => (
-            <SkillsCard photo={arr[1]} software={arr[0]} key={index} />
-          ))}
-        </div>
       </section>
       <style jsx>{`
         .aboutContainer {
@@ -49,25 +51,37 @@ export default function AboutSection() {
           text-decoration: underline;
         }
         .skillsContainer {
-          height: 30%;
+          height: 90%;
           width: 57%;
+          margin-left: 5%;
+          padding-top: 3%;
           display: flex;
           flex-direction: row;
           flex-wrap: wrap;
+          justify-content: center;
         }
         .bioImg {
-          width: 25%;
-          height: 40%;
+          width: 28%;
+          height: 100%;
           border: 20px solid white;
           border-radius: 25px;
-          margin-bottom: 2%;
+          margin-left: 5%;
           box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
         }
         .bio {
           width: 70%;
           font-size: 24px;
           margin-bottom: 4%;
+          margin-top: 8%;
           font-family: "Indie Flower", cursive;
+        }
+        .bioAndSkillsContainer {
+          height: 45%;
+          display: flex;
+          flex-direction: row;
+
+          justify-content: center;
+          align-items: center;
         }
       `}</style>
     </>
